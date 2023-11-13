c081cc5b5629a9b1a3956cc03de9423a3a268935
Jhonatan Abdala <jhonatan.jamir.abdala@gmail.com>
2023-11-13 15:01:16 -0300

    agrego script start

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-app.use(express.static(path.join(__dirname, '/public')))
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
-})
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'))
-})
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(path.join(__dirname, "/public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/login.html"));
 });
-app.listen(3000)
+app.listen(process.env.PORT, () => {
+  console.log(
+    `El servidor está corriendo en: http://localhost:${process.env.PORT}`
+  );
+});
